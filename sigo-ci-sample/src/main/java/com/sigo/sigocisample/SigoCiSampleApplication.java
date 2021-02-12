35cdd866f9aaae36bd0544b9159c6ebd654c78eb
Ramon <ramonrune@gmail.com>
2021-02-11 21:10:08 -0300

    Sample ci

---
+++ sigo-ci-sample/src/main/java/com/sigo/sigocisample/SigoCiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sigo.sigocisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigoCiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigoCiSampleApplication.class, args);
+	}
+
+}
---
+++ sigo-ci-sample/src/test/java/com/sigo/sigocisample/SigoCiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigo.sigocisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigoCiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
